fa31a875d84c180038363e3e0e5502ad1f337664
June YAMAMOTO <yamamoto@questetra.com>
2018-06-27 17:59:34 +0900

    0.0.2

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const ColorComverter = {
-  HexToRGB : function(hex) {
+  HexToRGB: function(hex) {
     var r = parseInt(hex.substr(1, 2), 16);
     var g = parseInt(hex.substr(3, 2), 16);
     var b = parseInt(hex.substr(5, 2), 16);
     return { r: r, g: g, b: b };
+  },
+  HexToHSL: function(hex) {
+    var rgb = ColorComverter.HexToRGB(hex);
+    return ColorComverter.RgbToHsl(rgb.r, rgb.g, rgb.b);
+  },
+  RgbToHsl: function(r, g, b) {
+    (r /= 255), (g /= 255), (b /= 255);
+    var max = Math.max(r, g, b),
+      min = Math.min(r, g, b);
+    var h,
+      s,
+      l = (max + min) / 2;
+
+    if (max == min) {
+      h = s = 0; // achromatic
+    } else {
+      var d = max - min;
+      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+      switch (max) {
+        case r:
+          h = (g - b) / d + (g < b ? 6 : 0);
+          break;
+        case g:
+          h = (b - r) / d + 2;
+          break;
+        case b:
+          h = (r - g) / d + 4;
+          break;
+      }
+      h /= 6;
+    }
+
+    return { h: h, s: s, l: l };
   }
-}
+};
 
 module.exports = ColorComverter;
--- test/test-index.js
+++ test/test-index.js
@@ -1,5 +1,5 @@
 var ColorComverter = require('../index.js');
 
-console.log(ColorComverter);
-
-console.log(ColorComverter.HexToRGB('#FFFFFF'));
+console.log('HexToRGB', '#FFFFFF', ColorComverter.HexToRGB('#FFFFFF'));
+console.log('HexToHSL', '#FF0000', ColorComverter.HexToHSL('#FF0000'));
+console.log('RgbToHsl', '256,0,0', ColorComverter.RgbToHsl(256,0,0));
